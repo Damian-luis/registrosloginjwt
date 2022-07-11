2c34a2a4286450d30425432f45cff22cfd216ebf
Damian-luis <damian.luis.porta@gmail.com>
2022-07-11 17:32:45 -0300

    gettin token

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import axios from 'axios';
 import { useState } from 'react';
-
+import { useNavigate } from "react-router-dom";
 export default function Login(){
 
-
+    const nav = useNavigate();
 //Login con logica Simple
 
 const [user,setUser]=useState({name:"",password:"",})
@@ -36,7 +36,7 @@ async function handlerSend(e){
 
 
 
-
+const [token,setToken]=useState("")
 const [estado,setEstado] =useState({
     user:"",
     password:"",
@@ -44,8 +44,7 @@ const [estado,setEstado] =useState({
 //Handler de envio
 async function mandar(e){
     e.preventDefault()
-    console.log(estado)
-    await axios.post("http://localhost:3001/login",estado).then(e=>{console.log(e.data)})
+    await axios.post(`http://localhost:3001/login/${estado.user}/${estado.password}`,estado).then(e=>{if (e){nav("/home")}})
     setEstado({ user:"",
     password:""})
     }
@@ -60,6 +59,85 @@ function pass(e){
     }
 
 
+
+
+
+
+
+
+
+//Registro
+const [register,setRegister]=useState({
+    user:"",
+    password:"",
+    name:"",
+    lastname:"",
+    age:"",
+    gender:""
+})
+async function handlerRegister(e){
+    e.preventDefault()
+    await axios.post("http://localhost:3001/add",register).then(e=>{console.log(e)})
+    setRegister({
+        user:"",
+    password:"",
+    name:"",
+    lastname:"",
+    age:"",
+    gender:""
+    })
+}
+function regUser(e){
+    setRegister({...register,user:e.target.value})
+}
+function regPassword(e){
+    setRegister({...register,password:e.target.value})
+}
+function regName(e){
+    setRegister({...register,name:e.target.value})
+}
+function regLastname(e){
+    setRegister({...register,lastname:e.target.value})
+}
+function regGender(e){
+    setRegister({...register,gender:e.target.value})
+}
+function regAge(e){
+    setRegister({...register,age:e.target.value})
+}
+
+
+
+
+
+
+
+//Aqui Estados y metodo de Compras
+
+const [compra,setCompra]=useState({
+    product:"",
+    price:"",
+    description:""
+})
+async function handlerCompra(e){
+    e.preventDefault();
+    const config={headers:{Authorization: `Bearer ${token}`}}
+    await axios.post("http://localhost:3001/login/verify",compra,config)
+    setCompra({
+        product:"",
+    price:"",
+    description:""
+    })
+}
+function productCompra(e){
+    setCompra({...compra,product:e.target.value})
+}
+function priceCompra(e){
+    setCompra({...compra,price:e.target.value})
+}
+function descriptionCompra(e){
+    setCompra({...compra,description:e.target.value})
+}
 return <div>
     <form onSubmit={handlerSend}>
         <input type="text" placeholder="ingrese usuario" onChange={nameHandler}/>
@@ -85,5 +163,33 @@ return <div>
      </form>
 
      {useractive==null? <h4>Por favor inicie sesion</h4>:<h2>Bienvenido {useractive[0].user}</h2>}
+
+
+
+     <hr/>
+     <hr/>
+     <h1>Registro de Usuario</h1>
+
+
+     <form onSubmit={handlerRegister}>
+        <input type="text" placeholder="ingrese usuario" onChange={regUser} value={register.user}/>
+        <input type="text" placeholder="ingrese contraseña" onChange={regPassword}  value={register.password}/>
+        <input type="text" placeholder="ingrese su nombre" onChange={regName}  value={register.name}/>
+        <input type="text" placeholder="ingrese su apellido" onChange={regLastname}  value={register.lastname}/>
+        <input type="text" placeholder="ingrese su edad" onChange={regAge}  value={register.age}/>
+        <input type="text" placeholder="ingrese su genero" onChange={regGender}  value={register.gender}/>
+        <button type="submit" >Registrarse</button>
+     </form>
+
+
+     <hr/>
+     <h1>Hacer post mandando y autorizando con el token</h1>
+     
+        <form onSubmit={handlerCompra}>
+            <input type="text" placeholder="ingrese nombre producto" onChange={productCompra} value={compra.product}></input>
+            <input type="text" placeholder="ingrese precio del producto" onChange={priceCompra} value={compra.price}></input>
+            <input type="text" placeholder="ingrese descripcion producto" onChange={descriptionCompra} value={compra.description}></input>
+            <button type="submit">Registrar compra</button>
+        </form>
 </div>
 }
